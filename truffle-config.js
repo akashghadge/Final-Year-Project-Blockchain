975fec4f57966b8b937ea64235b0cfbaa2d77318
Darshan Ahire <72602965+darshanahire@users.noreply.github.com>
2022-11-30 12:00:27 +0530

    initial deployment dommit

--- truffle-config.js
+++ truffle-config.js
@@ -5,22 +5,35 @@ const HDWalletProvider = require("@truffle/hdwallet-provider");
 const { SEED_PHRASE, INFURA_KEY } = require("./seed-phrase");
 const infuraKey = INFURA_KEY;
 const seedPhrase = SEED_PHRASE;
-
 module.exports = {
   networks: {
-    development: {
-      host: "127.0.0.1", // Localhost (default: none)
-      port: 9545, // Standard Ethereum port (default: none)
-      network_id: "*", // Any network (default: none,
-    },
-    rinkeby: {
-      provider: () => new HDWalletProvider(seedPhrase, infuraKey),
-      network_id: 4,
-      gas: 5500000,
-      confirmations: 2,
-      timeoutBlocks: 200000000000000,
-      skipDryRun: true,
-    },
+      development: {
+        host: "127.0.0.1", // Localhost (default: none)
+        port: 9545, // Standard Ethereum port (default: none)
+        network_id: "*", // Any network (default: none,
+      },
+      goerli: {
+        provider: () => new HDWalletProvider(seedPhrase, 'https://goerli.infura.io/v3/'+infuraKey),
+        network_id: 5,
+        gas: 5500000,
+        confirmations: 2,
+        skipDryRun: true,
+      },
+      sepolia: {
+        provider: () => new HDWalletProvider(seedPhrase, 'https://sepolia.infura.io/v3/'+infuraKey),
+        network_id: 11155111,
+        gas: 5500000,
+        confirmations: 2,
+        skipDryRun: true,
+      },
+      rinkeby: {
+        provider: () => new HDWalletProvider(seedPhrase, 'https://rinkeby.infura.io/v3/'+infuraKey),
+        network_id: 4,
+        gas: 5500000,
+        confirmations: 2,
+        timeoutBlocks: 200000000000000,
+        skipDryRun: true,
+      },
   },
   contracts_directory: "./src/contracts/",
   contracts_build_directory: "./src/abis/",
