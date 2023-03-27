07739383053c8f4777e4b74f1a24e3fbb71e3fff
Darshan Ahire <72602965+darshanahire@users.noreply.github.com>
2023-03-27 23:36:18 +0530

    Certificate Background changes

--- src/components/GenerateCertificateModal.js
+++ src/components/GenerateCertificateModal.js
@@ -22,24 +22,23 @@ function GenerateCertificateModal(props) {
         <Modal size="large" className="modal-des" open={props?.isOpen}>
             <Modal.Content className="modal-content">
                 <div className="container pm-certificate-container">
-                    <div className="outer-border" style={props.certificateDetails.isScored ? { background:"#618597"} : {background:"#964B00"}} ></div>
-                    <div className="inner-border"></div>
-                    <div className="pm-certificate-border col-xs-12">
+                    <div className="pm-certificate-border col-xs-12 certBackground">
                         <div className="row pm-certificate-header">
-                            <div className="custom-logo-container">
-                                <img src={orgData.OrgLogo} height="150px" width="150px"></img>
-                            </div>
-                            <div className="pm-certificate-title cursive col-xs-12 text-center">
+                            {/* <div className="pm-certificate-title cursive col-xs-12 text-center" style={{marginTop : "120px"}}>
                                 <h2>{orgData.name}</h2>
-                            </div>
+                            </div> */}
                         </div>
-                        <div className="row pm-certificate-body">
-                            <div className="pm-certificate-block">
+                        <div className="row pm-certificate-body"  style={{marginTop: "110px"}}>
+                            <div className="pm-certificate-block" >
                                 <div className="col-xs-12">
                                     <div className="row">
                                         <div className="col-xs-2"></div>
-                                        <div className="pm-certificate-name underline margin-0 col-xs-8 text-center">
+                                        <div className="pm-certificate-name margin-0 col-xs-8 text-center d-flex flex-column justify-content-center" style={{alignItems:"center"}}>
+                                            <h2 style={{margin:"7px"}}>This Cerificate is awarded to </h2>
                                             <span className="pm-name-text bold">{props.employee.name}</span>
+                                            <div style={{width:"70%"}}>
+                                            <h4 style={{marginTop:"13px"}}>For Successfully Completing <b>{orgData.name} </b>Institutes Online <b>{props.certificateDetails.certificate_type}</b> Certification Course  </h4>
+                                            </div>
                                         </div>
                                         <div className="col-xs-2"></div>
                                     </div>
@@ -55,16 +54,19 @@ function GenerateCertificateModal(props) {
                                                             <div className="row">
                                                                 <div className="col-xs-2"></div>
                                                                 <div className="pm-earned col-xs-8 text-center">
-                                                                    <span className="pm-earned-text padding-0 block cursive font25 bold">has earned</span>
+                                                                    <h4 style={{margin:0}}>With Score of </h4>
                                                                     <span className="pm-credits-text block bold sans">{props.certificateDetails.score}</span>
                                                                 </div>
                                                                 <div className="col-xs-2"></div>
                                                                 <div className="col-xs-12"></div>
                                                             </div>
                                                         </div>
+                                                        <div className="custom-logo-container mt-2s">
+                                                            <img src={orgData.OrgLogo} height="100px" width="100px"></img>
+                                                        </div>
                                                     </> : null
                                             }
-                                            <div className="col-xs-12">
+                                            {/* <div className="col-xs-12">
                                                 <div className="row">
                                                     <div className="col-xs-2"></div>
                                                     <div className="pm-course-title col-xs-8 text-center">
@@ -76,12 +78,12 @@ function GenerateCertificateModal(props) {
                                             <div className="col-xs-12">
                                                 <div className="row">
                                                     <div className="col-xs-2"></div>
-                                                    <div className="pm-course-title underline col-xs-8 text-center">
+                                                    <div className="pm-course-title col-xs-8 text-center">
                                                         <span className="pm-credits-text block bold sans">{props.certificateDetails.name} [{props.certificateDetails.certificate_type}]</span>
                                                     </div>
                                                     <div className="col-xs-2"></div>
                                                 </div>
-                                            </div>
+                                            </div> */}
                                         </> :
                                         <>
                                             <div className="col-xs-12">
