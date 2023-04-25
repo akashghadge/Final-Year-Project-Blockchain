d4975e8aca22fb9b67f56b55c07800376bb1681e
akash_ghadge <asghadge6@gmail.com>
2023-04-25 10:54:45 +0530

    Home Component Tests Done

--- src/components/__test__/Home.test.js
+++ src/components/__test__/Home.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import Home from '../Home';
-
-test('renders learn react link', () => {
-    render(<Home />);
-    const linkElement = screen.getByText(/BlockChain based/i);
-    expect(linkElement).toBeInTheDocument();
-});
+describe("Home Test Suit", () => {
+    test('Test for heading', () => {
+        render(<Home />);
+        const linkElement = screen.getByText(/BlockChain based/i);
+        expect(linkElement).toBeInTheDocument();
+    });
+    test('Test for button', () => {
+        render(<Home />);
+        const buttonElement = screen.getByRole('button', { name: /Download Metamask Extension/i })
+        expect(buttonElement).toBeInTheDocument();
+    });
+})
